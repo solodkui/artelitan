5cc0d84a5724af3f3ab5c3983322aa09e8b697e4
solodkui <solodkuiA@gmail.com>
2021-04-12 18:57:34 +0300

    Dev process

---
+++ src/app/modules/main/main-slider/main-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-slider',
+  templateUrl: './main-slider.component.html',
+  host: { class: 'c-main-slider' },
+})
+export class MainSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { MainRoutingModule } from './main.routing';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MainSliderComponent } from './main-slider/main-slider.component';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, MainSliderComponent],
   imports: [
     CommonModule,
     MainRoutingModule,
